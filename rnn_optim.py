dd447c9e42705845cbcdebe12f263f01740cd43c
vimarshc <vimarsh chaturvedi>
2019-02-09 11:27:33 +0530

    Added base version of David.et al.

---
+++ rnn_optim.py
@@ -0,0 +1,39 @@
+from torch import nn
+from torch._jit_internal import weak_module, weak_script_method
+from torch.nn import functional as F
+
+
+@weak_module
+class Normstabilizer(nn.NLLLoss):
+	"""
+	The norm-stabilizer was introduced by David et.al
+	"""
+
+
+	__constants__ = ['ignore_index', 'weight', 'reduction','norm_stabilizer_param']
+
+	def __init__(self, weight=None, size_average=None, ignore_index=-100,
+				 reduce=None, reduction='mean',norm_stabilizer_param=None):
+		super(Normstabilizer, self).__init__(weight, size_average, reduce, reduction)
+		self.ignore_index = ignore_index
+		self.norm_stabilizer_param = norm_stabilizer_param
+		self.euclid_norm = nn.PairwiseDistance(p=2)
+
+	@weak_script_method
+	def forward(self, input, target,hidden_layers):
+		sequence_length = hidden_layers.shape[0]
+
+		h_y = hidden_layers.norm(dim=2)
+		h_x = torch.roll(h_y,-1,0)
+		
+		h_y = h_y[:-1].flatten()
+		h_x = h_x[:-1].flatten()
+
+
+		_nllloss = F.nll_loss(input,target,weight=self.weight,ignore_index=self.ignore_index,reduction=self.reduction)
+		_normstabilizer_loss = F.mse_loss(h_x,h_y,size_average=False,reduce=True,reduction='sum')
+
+		_hyperparam = torch.autograd.Variable(torch.tensor(self.norm_stabilizer_param/sequence_length), requires_grad=True)
+
+		return _nllloss + _hyperparam*_normstabilizer_loss 
+
